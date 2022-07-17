6050dc0fbe0ee998527b92f36daefa7c6bb47706
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2022-07-17 15:57:47 +0530

    Add files via upload

---
+++ basicfunction.js
@@ -0,0 +1,11 @@
+function showAlert(msg){
+    alert(msg);
+}
+
+function sum(a,b){
+    console.log(a+b);
+    return a+b;
+}
+var result=sum(4,5,6);//last number will be ognored
+console.log(result);
+//showAlert("heloo!!");
