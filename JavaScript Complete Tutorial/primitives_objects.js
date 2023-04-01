6ddc744bce5d7207bfd6ffd5bb2786d85a3f49b7
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2023-04-01 21:38:22 +0530

    Create primitives_objects.js

---
+++ JavaScript Complete Tutorial/primitives_objects.js
@@ -0,0 +1,23 @@
+// there are 7 promitive data types in javascript
+// object is a non- primitive datatype in javascript
+//nn bb ss u
+//Primitives in js
+let a =null;
+let b=345;
+let c=true;// can also be false
+let d=BigInt("567")+BigInt("3")
+let e="Harry"
+let f=Symbol("I am a nice symbol")
+let g=undefined
+console.log(a,b,c,d,e,f,g)
+console.log(typeof c)
+
+//objects in js: non-promitive datatype
+const item={
+  "Harry": true,
+  "Shubh": false,
+  "Lovish":67,
+  "Rohan": undefined
+}
+console.log(item["Shubh"])
+
