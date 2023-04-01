410b2da683224ebf98b1ad1f58d5cb6778a6aa46
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2023-04-01 21:35:25 +0530

    Create for_loop.java

---
+++ JavaScript Complete Tutorial/for_loop.java
@@ -0,0 +1,26 @@
+// Program to add the first n natural number
+/*
+let sum =0
+let n=prompt("Enter the value if n")
+n=Number.parseInt(n)
+for(let i=0;i<n;i++){
+  sum+=(i+1)
+  console.log((i+1),"+")
+}
+console.log("Sum of first"+n+ "natural number is "+sum)
+*/
+
+let obj={
+  harry:90,
+  shubh:45,
+  shivika:56,
+  ritka:58,
+  shiv:23
+}
+for(let a in obj){
+  console.log("Marks of " + a + " are "+  obj[a])
+}
+
+for(let b of "harry"){
+  
+}
