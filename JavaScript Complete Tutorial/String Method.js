31cd51bb28489d8533680ce9882245df616a86f5
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2023-04-01 21:32:15 +0530

    Create String Method.js

---
+++ JavaScript Complete Tutorial/String Method.js
@@ -0,0 +1,23 @@
+let name="Ananya"
+console.log(name.length)// calculate the length of the string
+
+// if escape sequence character is presnt in the string then it not included in the string length
+console.log(name.toUpperCase())// conver the whole string to the upper case
+
+console.log(name.toLowerCase())// convert the string to the lower case
+
+console.log(name.slice(2,4))// index of the string start from the zero
+
+console.log(name.slice(2))
+
+console.log(name.replace("Ana","Anu"))// it replace character to another charater
+
+// if character is  not present then it will not replace
+
+console.log(name.trim()) // remove the white spaces
+
+let friend ="Anant"
+console.log(name.concat(" is a friend of ", friend, "Ok"))
+// concat is used to concat the two strings
+
+//Strings are the immutabe datatype,
