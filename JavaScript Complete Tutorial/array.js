b13da0f0781e107d9cc6f1587773f080b99724f7
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2023-04-02 01:06:32 +0530

    Create array.js

---
+++ JavaScript Complete Tutorial/array.js
@@ -0,0 +1,22 @@
+//arrays are variables which can hold more than one value
+
+let marks_class_12 =[1,2,3,4,5,null,false]
+console.log(marks_class_12)
+
+//accessing the values in the array
+console.log(marks_class_12[3])
+
+// to calculate the length if the array
+console.log(marks_class_12.length)
+
+// Adding the  new values in the array
+marks_class_12[6]=90
+console.log(marks_class_12)
+
+//Arrays are mutable and can be changed
+
+// in javascript array are objects
+
+console.log(typeof(marks_class_12))
+
+
