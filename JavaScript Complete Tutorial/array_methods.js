8ec807c93f052c90450facffae6422f6c8e3c157
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2023-04-02 01:05:30 +0530

    Create array_methods.js

---
+++ JavaScript Complete Tutorial/array_methods.js
@@ -0,0 +1,22 @@
+//Array methods
+let num=[1,2,3,4,56]
+// converting the array to string
+let b=num.toString()
+console.log(b)
+// join is used to join the arrya and return the string
+let c=num.join("_")
+console.log(c)
+// pop is used to remove the last element if the array
+let r=num.pop()
+console.log(num)
+console.log(r)// return the popped element
+
+// push returns the new array length
+let s=num.push(78)
+console.log(num)
+
+// shift is used to remove the element from the starting
+
+let p=num.shift(4)
+console.log(p, num)
+
