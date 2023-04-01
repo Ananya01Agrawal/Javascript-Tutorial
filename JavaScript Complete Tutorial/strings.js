2aec8dd6b7f2f221e14b0717ae41560854616f3c
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2023-04-01 17:16:00 +0530

    Create strings.js

---
+++ JavaScript Complete Tutorial/strings.js
@@ -0,0 +1,35 @@
+let name="Ananya" // strings are created  using the following syntax
+console.log(name.length)// used in calculating the length if the string
+
+console.log(name[0])// gives the first character if the string
+
+console.log(name[1])// gives the second character of the string
+
+//Strings are use to store and manipulate the text
+
+// string can also be created using the single quote 
+let friend='Prakash'
+console.log(friend)
+
+// Template Literal
+let boy1="Pramod"
+let boy2="Nikhil"
+// Nikhil is a friend oif Pramod
+let sentence =`${boy1}  is a friend of ${boy2}`
+console.log(sentence)
+
+// what is string interpulation
+//we can isert the variable directly in template literal
+
+
+// Escape Sequence Characters
+let fruit='Bana\n ana' // come in the new line
+console.log(fruit)
+
+let veg="Brin\"jle"
+console.log(veg)
+
+// \n = new line
+// \t = tab
+
+
