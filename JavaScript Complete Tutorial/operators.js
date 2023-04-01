8180ae094fdc49252854c9b2f702b69c9ecf4011
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2023-04-01 21:34:13 +0530

    Create operators.js

---
+++ JavaScript Complete Tutorial/operators.js
@@ -0,0 +1,37 @@
+console.log(" Arithmetic operators in JS")
+let a=10;
+let b=4;
+console.log("a+b=",a+b)
+console.log("a-b=",a-b)
+console.log("a/b=",a/b)
+console.log("a**b=",a**b)
+console.log("a%b=",a%b)
+console.log("++a =",++a) //11
+console.log("a++ =",a++)//11
+//a=12
+console.log("--a =",--a)//11
+console.log("++a =",++a)//12
+console.log("a= ",a)//12
+console.log("a--",a--)//12
+
+//Assignemnt operators
+a=8// we initilise vlue of a  with 8
+a+=5// same as a=a+5
+console.log(a)
+
+
+//comparison operators
+//== equal to
+// if something is true we return the true else we return the false
+let comp1=6;
+let comp2=6;
+console.log("comp1==comp2 is ", comp1==comp2)
+console.log("comp1== comp2 is", comp1!=comp2)
+console.log("comp1=== comp2 is",comp1===comp2)
+
+//logical operators
+let x=5;
+let y=6;
+console.log(x>y &&x==5)
+console.log(x>y ||x==5)
+console.log(!false)
